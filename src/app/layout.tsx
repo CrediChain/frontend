664213cf8b0a39e11a6b789daf6fe61562e84a18
Riiz0 <shawnrizo@hotmail.com>
2024-10-07 19:18:04 -0400

    Feat: Base Connect Wallet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { headers } from 'next/headers';
+import { type ReactNode } from 'react';
+import { cookieToInitialState } from 'wagmi';
 import localFont from "next/font/local";
+import '@coinbase/onchainkit/styles.css';
 import "./globals.css";
+
+import { getConfig } from '../config';
+import { Providers } from '../providers';
 import Layout from "./components/Layout";
 
 const geistSans = localFont({
@@ -8,6 +15,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -21,13 +29,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: ReactNode;
+}) {
+  const initialState = cookieToInitialState(
+    getConfig(),
+    headers().get('cookie')
+  );
+
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Layout>{children}</Layout>
+      <body className={`${geistSans.variable} ${geistMono.variable} font-sans bg-background text-foreground`}>
+        <Providers initialState={initialState}>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   );
